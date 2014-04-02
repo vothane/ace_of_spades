3b5bab36f7c52a65beca8c2af4fe5bdf347d556f
Thane Vo <vothane@gmail.com>
2014-04-02 00:23:31 -0600

    refactor query DSL test for better readability

--- spec/query_spec.rb
+++ spec/query_spec.rb
@@ -1,26 +1,25 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-module MockQuery
-  def self.conditions(&block)
+module MockSearch
+  def self.search(&block)
     conditions = Query::Conditions.new(&block)
   end
 end 
 
-describe "query" do
-
+describe "query DSL" do
   context "using the dsl in mocked search" do
 
     let(:dsl_query_matching) do
-      MockQuery::conditions do |dummy|
-         dummy.name == "Miles"
-         dummy.hobby == "couch-surfing"
+      MockSearch::search do |search|
+         search.name == "Miles"
+         search.hobby == "couch-surfing"
       end     
     end
 
     let(:dsl_query_fuzzy) do
-      MockQuery::conditions do |dummy|
-         dummy.name =~ "Mi"
-         dummy.hobby =~ "surf"
+      MockSearch::search do |search|
+         search.name =~ "Mi"
+         search.hobby =~ "surf"
       end     
     end
 
@@ -31,5 +30,6 @@ describe "query" do
     it "build correct query string for fuzzy search" do
       dsl_query_fuzzy.to_search_conditions.should == "name~'Mi' AND hobby~'surf'"
     end 
+
   end 
 end      
